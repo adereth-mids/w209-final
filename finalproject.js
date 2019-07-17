41196c70e455631946279b0358fd34ab5e6e5764
Matt Adereth <adereth@gmail.com>
2019-07-17 16:37:21 -0700

    Showing graph up to episode

--- finalproject.js
+++ finalproject.js
@@ -1,7 +1,112 @@
+var getCharacterCount = function(episode) {
+    characters = {}
+    episode.scenes.forEach(function(scene) {
+	scene.characters.forEach(function(c) {
+	    characters[c.name] = 1
+	})
+    })
+    return Object.keys(characters).length
+
+}
+
+var generateCharacterCountPlot = function(data) {
+
+    var margin = {top: 20, right: 20, bottom: 50, left: 80},
+	    width = 960 - margin.left - margin.right,
+	    height = 480 - margin.top - margin.bottom;
+
+	var plot = d3.select("#characterCountPlot").append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+
+	var x = d3.scaleLinear().domain([1, data.episodes.length]).range([0, width]);
+	var y = d3.scaleLinear().domain([0, 100]).range([height, 0]);
+
+    plot.append("g")
+	.attr("transform", "translate(" + margin.left + "," + (height + margin.top) + ")")
+	.call(d3.axisBottom(x))
+
+    yAxis = plot.append("g")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+	.call(d3.axisLeft(y))
+
+    plot.append("text")
+	.attr("transform",
+	      "translate(" + (margin.left + width/2) + " ," +
+	      (height + 10 + margin.bottom) + ")")
+	.style("text-anchor", "middle")
+	.text("Episode");
+
+    plot.append("text")
+	.attr("transform", "rotate(-90)")
+	.attr("y", margin.left/2)
+	.attr("x",0 - (height / 2))
+	.attr("dy", "1em")
+	.style("text-anchor", "middle")
+	.text("Character Count");
+
+    plot.append("g").selectAll("rect").data(data.episodes).enter()
+	.append("rect")
+	.attr("x", function(d,i) {
+	    return x(i + 1) })
+	.attr("y", function(d,i) {return y(getCharacterCount(d)) })
+
+	.attr("width", x(2) - x(1))
+	.attr("height", function(d,i) {  return y(0) - y(getCharacterCount(d))})
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+	.style("fill", "steelblue")
+	.style("stroke", "#000000")
+	.on("click", function(d,i) {
+	    generateGraph(data, i);
+	})
+
+
+
+}
+
+var getGraphBetweenEpisodes = function(data, episodeStart, episodeEnd) {
+    relations = {}
+    characterAppearanceTime = {}
+    for (var i = episodeStart; i < episodeEnd; i++) {
+	data.episodes[i].scenes.forEach(function(scene) {
+	    sceneLength =
+		(new Date("1970-01-01 " + scene.sceneEnd)) -
+		(new Date("1970-01-01 " + scene.sceneStart));
+
+	    scene.characters.forEach(function(c1) {
+		if (characterAppearanceTime[c1.name]) {
+		    characterAppearanceTime[c1.name] = characterAppearanceTime[c1.name] + sceneLength;
+		} else {
+		    characterAppearanceTime[c1.name] = sceneLength;
+		}
+
+		scene.characters.forEach(function(c2) {
+		    if(c1.name != c2.name) {
+			var pair = [c1.name, c2.name].sort();
+			if (relations[pair]) {
+			    relations[pair] = relations[pair] + sceneLength;
+			} else {
+			    relations[pair] = sceneLength;
+			}}})})})
+    }
+
+    links = []
+    Object.keys(relations).forEach(function(relation) {
+	names = relation.split(",")
+	links.push({"source": names[0], "target": names[1], "value": relations[relation]})
+    })
+
+    nodes = []
+    Object.keys(characterAppearanceTime).forEach(function(character) {
+	nodes.push({"id": character, "screenTime" : characterAppearanceTime[character]})
+    })
+
+    return {"links" : links,
+	    "nodes" : nodes};
+}
 
 
 var getGraphForEpisode = function(data, episode) {
-
     relations = {}
     characterAppearanceTime = {}
     data.episodes[episode].scenes.forEach(function(scene) {
@@ -41,7 +146,8 @@ var getGraphForEpisode = function(data, episode) {
 }
 
 var generateGraph = function(data, episode) {
-    graphData = getGraphForEpisode(data, episode);
+    d3.select("#network").selectAll("svg").remove()
+    graphData = getGraphBetweenEpisodes(data, 0, episode);
 
     var margin = {top: 20, right: 20, bottom: 50, left: 80},
 	width = 960 - margin.left - margin.right,
@@ -54,7 +160,9 @@ var generateGraph = function(data, episode) {
     var simulation = d3.forceSimulation()
  	.force("link", d3.forceLink().id(function(d) { return d.id; }))
 	.force("charge", d3.forceManyBody())
-	.force("center", d3.forceCenter(width / 2, height / 2));
+	.force("center", d3.forceCenter(width / 2, height / 2))
+    //	.strength(+this.value)
+    ;
 
     function dragstarted(d) {
 	if (!d3.event.active) simulation.alphaTarget(0.3).restart();
@@ -133,112 +241,8 @@ var generateGraph = function(data, episode) {
 }
 
 
-var generateTimeLine = function(data) {
-    var margin = {top: 20, right: 20, bottom: 50, left: 80},
-	width = 960 - margin.left - margin.right,
-	height = 960 - margin.top - margin.bottom;
-
-
-    characterIdMap = {}
-    var currId = 0;
-
-    appearances = []
-    for (var i in data.episodes) {
-	episode = data.episodes[i]
-	scenes = episode.scenes
-
-	episodeLengthSeconds =
-	    (new Date("1970-01-01 " + scenes[scenes.length-1].sceneEnd)) -
-	    (new Date("1970-01-01 00:00"));
-	scenes.forEach(function(scene) {
-	    sceneStartFraction = (new Date("1970-01-01 " + scene.sceneStart) - new Date("1970-01-01 00:00")) / episodeLengthSeconds
-	    sceneEndFraction = (new Date("1970-01-01 " + scene.sceneStart)  - new Date("1970-01-01 00:00")) / episodeLengthSeconds
-	    scene.characters.forEach(function(c) {
-		if (characterIdMap[c.name]) {
-
-		} else {
-		    characterIdMap[c.name] = currId;
-		    currId = currId + 1;
-		}
-		appearances.push(
-		    {"character": c.name,
-		     "episodeLength": episodeLengthSeconds,
-		     "sceneStartTime": (new Date("1970-01-01 " + scene.sceneStart) - new Date("1970-01-01 00:00:00")),
-		     "sceneStartFraction": sceneStartFraction,
-		     "sceneEndFraction": sceneEndFraction,
-		     "sceneLength" : sceneEndFraction - sceneStartFraction,
-		     "sceneStart": scene.sceneStart,
-		     "sceneEnd": scene.sceneEnd,
-		     "episode": +i
-		    })
-	    })
-
-	})
-
-
-    }
-    console.log(currId)
-    var x = d3.scaleLinear().domain([0, 70]).range([0, width]);
-    var y = d3.scaleLinear().domain([0, currId]).range([0, height]);
-
-    var svg = d3.select("#timeline").append("svg")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom)
-
-    //svg.selectAll("rectangle").data(appearances.slice(0,1000)).enter()
-    svg.selectAll("rectangle").data(appearances).enter()
-	.append("rect")
-	.attr("x", function(d) { return x(d.episode + d.sceneStartFraction) })
-	.attr("y", function(d) {  return y(characterIdMap[d.character]) })
-	.attr("width", function(d) { return Math.max(1,x(d.sceneLength) - x(0)) })
-    //	.attr("width", 5)
-	.attr("height", y(0.5))
-	.attr("fill", "black")
-
-
-
-
-}
-/*
-  data.episodes[episode].scenes.forEach(function(scene) {
-  sceneLength =
-  (new Date("1970-01-01 " + scene.sceneEnd)) -
-  (new Date("1970-01-01 " + scene.sceneStart));
-
-  scene.characters.forEach(function(c1) {
-  if (characterAppearanceTime[c1.name]) {
-  characterAppearanceTime[c1.name] = characterAppearanceTime[c1.name] + sceneLength;
-  } else {
-  characterAppearanceTime[c1.name] = sceneLength;
-  }
-
-  scene.characters.forEach(function(c2) {
-  if(c1.name != c2.name) {
-  var pair = [c1.name, c2.name].sort();
-  if (relations[pair]) {
-  relations[pair] = relations[pair] + sceneLength;
-  } else {
-  relations[pair] = sceneLength;
-  }}})})})
-
-  links = []
-  Object.keys(relations).forEach(function(relation) {
-  names = relation.split(",")
-  links.push({"source": names[0], "target": names[1], "value": relations[relation]})
-  })
-
-  nodes = []
-  Object.keys(characterAppearanceTime).forEach(function(character) {
-  nodes.push({"id": character, "screenTime" : characterAppearanceTime[character]})
-  })
-
-*/
-
-
 
 d3.json("https://raw.githubusercontent.com/jeffreylancaster/game-of-thrones/master/data/episodes.json", function (data) {
-    d = data;
-	generateTimeLine(data);
-	generateGraph(data, 1);
-
+    generateCharacterCountPlot(data);
+	//	generateGraph(data, 1);
     })
