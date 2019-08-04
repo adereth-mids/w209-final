187c3630b7dfbe30c556098680e7a907de2150c2
Matt Adereth <adereth@gmail.com>
2019-08-03 17:34:17 -0700

    Font changes and scene visualization

--- finalproject.js
+++ finalproject.js
@@ -1,3 +1,42 @@
+var episodeData
+var characterDetails
+var appearances
+
+var toMillis = function(timeString) {
+    return (new Date("1970-01-01 " + timeString)) - (new Date("1970-01-01 " + "00:00"))
+}
+
+var getCharacterDetailsMap = function(characterData) {
+    var result = {}
+
+    characterData.characters.forEach(function(c) {
+	result[c.characterName] = c
+    })
+    return result
+}
+
+var populateAppearances = function(episodes) {
+    var characterAppearances = {}
+    for (var i = 0; i < episodes.length; i++) {
+	var sceneCount = episodes[i].scenes.length
+
+	var episodeLength = toMillis(episodes[i].scenes[sceneCount - 1].sceneEnd)
+	episodes[i].scenes.forEach(function(scene) {
+	    scene.characters.forEach(function(c) {
+		if(!characterAppearances[c.name]) {
+		    characterAppearances[c.name] = []
+		}
+		characterAppearances[c.name].push(
+		    {"sceneStart" : (toMillis(scene.sceneStart) / episodeLength) + i,
+		     "sceneEnd": (toMillis(scene.sceneEnd) / episodeLength) + i,
+		     "details" : scene});
+
+	    })
+	})}
+    return characterAppearances
+}
+
+
 var characterHouseMap = {}
 
 // Color selection informed by http://hauteslides.com/2011/05/game-of-thrones-infographic-illustrated-guide-to-houses-and-character-relationships/
@@ -38,23 +77,29 @@ var generateHouseLegend = function() {
     var legend = d3.select("#houselegend").append("svg")
 	.attr("width", width + margin.left + margin.right)
 	.attr("height", height + margin.top + margin.bottom)
-    var x = d3.scaleLinear().domain([0, Object.keys(houseColor).length - 1]).range([margin.left, width - margin.right]);
+    	.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+    var x = d3.scaleLinear()
+	.domain([0, Object.keys(houseColor).length - 1])
+	.range([margin.left, width - margin.right]);
+
     legend.selectAll("circle").data(Object.keys(houseColor))
 	.enter()
 	.append("circle")
     	.attr("r", 9)
 	.attr("cx", function(d,i) {return x(i)})
-	.attr("cy", height / 2)
+	.attr("cy", function(d,i) { return height / 2 + (i % 2) * 12})
 	.attr("fill", function(d) {return houseColor[d]})
 
     legend.selectAll("text").data(Object.keys(houseColor))
 	.enter()
 	.append("text")
 	.attr("x", function(d,i) {return x(i)})
-	.attr("y", height / 2 + 18)
-	.text(function(d) { return d })
+    	.attr("y", function(d,i) { return height / 2 + (i % 2) * 12 + 18})
+
+	.text(function(d) { return d.replace(" ", "\n") })
 	.attr("text-anchor", "middle")
-	.attr("font-size", 9)
+	.attr("font-size", 8)
 
 
 }
@@ -106,6 +151,7 @@ var getCharacterCount = function(episode) {
 	    .attr("x",0 - ((margin.top + height) / 2))
 	    .attr("dy", "1em")
 	    .style("text-anchor", "middle")
+	    .attr("font-size", 14)
 	    .text("Character Count");
 
 	plot.append("g").selectAll("rect").data(data.episodes).enter()
@@ -123,13 +169,13 @@ var getCharacterCount = function(episode) {
 		//BG Update increased to i+1
 		generateGraph(data, i+1);
 	    })
-	// BG update to include tooltip
+	    .style("cursor", "pointer")
 	    .append("plot:title")
 	    .text(function(d) { return "S" + d.seasonNum + "E" + d.episodeNum + " - " + d.episodeTitle + ": " + d.episodeDescription; });
     }
 
-    var getGraphBetweenEpisodes = function(data, episodeStart, episodeEnd) {
-	relations = {}
+var getGraphBetweenEpisodes = function(data, episodeStart, episodeEnd) {
+    relations = {}
 	characterAppearanceTime = {}
 	for (var i = episodeStart; i < episodeEnd; i++) {
 	    data.episodes[i].scenes.forEach(function(scene) {
@@ -274,23 +320,15 @@ var getCharacterCount = function(episode) {
 	    d.fy = null;
 	}
 
-
-	//console.log(graphData)
-
-	//	color = d3.scaleOrdinal(d3.schemeCategory20) // Li: add color
-
 	var link = svg.append("g")
 	    .attr("class", "links")
 	    .selectAll("line")
 	    .data(graphData.links)
 	    .enter().append("line")
-	//.attr("stroke-width", function(d) { return 0.5; })
     	    .attr("stroke", function(d) { return "#000000"; })
 	    .style("opacity", 0.5);
 
-
-	// BG update to calculate max screen time for node radius
-	maxtime = d3.max(graphData.nodes, function(d) { return d.screenTime; });
+	var maxtime = d3.max(graphData.nodes, function(d) { return d.screenTime; });
 
 	var node = svg.append("g")
 	    .attr("class", "nodes")
@@ -299,20 +337,17 @@ var getCharacterCount = function(episode) {
 	    .enter().append("g")
 
 	var circles = node.append("circle")
-	// BG update to radius based on screen time
 	    .attr("r", function(d) { return 5+30*d.screenTime/maxtime; })
-	//Li .attr("fill", function(d) { return "#ff0000"; })
 	    .attr("fill", function(d) {
-		//		console.log([d.id, characterHouseMap[d.id]])
 		if (houseColor[characterHouseMap[d.id]]) {
 		    return houseColor[characterHouseMap[d.id]];
 		} else {
 		    return houseColor["Other"]
 		}
 
-	    })// Li: color per house -->d.house not working
+	    })
 	    .style("opacity", 0.8) // Li: add color opacity
-
+	    .style("cursor", "pointer")
 	// BG update increase radius when mouse over
 	    .on("mouseover", function(d,i) {d3.select(this)
 					    .attr("r", 40)
@@ -326,6 +361,7 @@ var getCharacterCount = function(episode) {
 	//BG Update to highlight subnetwork
 	    .on('click', function(d) {
 		_displayConnections(d.id);
+		generateCharacterScenePlot(d.id)
 		// d3.event.stopPropagation();
 	    });
 	//BG Update
@@ -353,8 +389,8 @@ var getCharacterCount = function(episode) {
 	    .text(function(d) {
 		return d.id;
 	    })
-	    .style("font-size", function(d){return 5+8*d.screenTime/maxtime;}) // Li: add text size
-	    .attr("fill","grey48") // Li: add text color
+	    .style("font-size", function(d){return 5+8*d.screenTime/maxtime;})
+	    .attr("fill","grey48")
 	    .attr('x', 6)
 	    .attr('y', 3);
 
@@ -382,20 +418,127 @@ var getCharacterCount = function(episode) {
 		    return "translate(" + d.x + "," + d.y + ")";
 		})
 	}
-
     }
 
 
-    var ready = function(_, episodes, groups) {
+    var generateCharacterScenePlot = function(mainCharacter) {
+	d3.select("#timeline").selectAll("svg").remove()
+
+	var relationCount = 20
+	var getRelatedCharacters = function(character) {
+	    var r = {}
+	    appearances[character].forEach(function(scene) {
+		scene.details.characters.forEach(function(c) {
+		    if(!r[c.name]) {
+			r[c.name] = scene.sceneEnd - scene.sceneStart
+		    } else {
+			r[c.name] = r[c.name] + scene.sceneEnd - scene.sceneStart
+		    }
+
+		})
+	    })
+	    return Object.keys(r).sort(function(a,b) { return r[b] - r[a] })
+	}
+
+	var sceneContainsCharacter = function(scene, character) {
+	    for (var i = 0; i < scene.details.characters.length; i++) {
+		if (scene.details.characters[i].name == character) {
+		    return true;
+		}
+
+	    }
+	    return false;
+	}
+	var margin = {top: 20, right: 20, bottom: 50, left: 160},
+	    width = 960 - margin.left - margin.right,
+	    height = 370 - margin.top - margin.bottom;
+
+	var plot = d3.select("#timeline").append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+
+	var x = d3.scaleLinear().domain([1, episodeData.episodes.length]).range([0, width]);
+	var y = d3.scaleLinear().domain([0, relationCount]).range([height, 0]);
+
+	plot.append("g")
+	    .attr("transform", "translate(" + margin.left + "," + (height + margin.top) + ")")
+	    .call(d3.axisTop(x))
+	plot.append("text")
+	    .attr("transform",
+		  "translate(" + (margin.left + width/2) + " ," +
+		  (height + 10 + margin.bottom) + ")")
+	    .style("text-anchor", "middle")
+	    .text("Episode");
+	related = getRelatedCharacters(mainCharacter).slice(0,relationCount)
+	console.log(related)
+	plot.append("g").selectAll("text").data(related).enter().append("text")
+	    .attr("y", function(d,i) { return 9 + y(relationCount - 0.5 - i) })
+	    .attr("x", x(15))
+	    .attr("dy", "1em")
+    	    .style("text-anchor", "end")
+	    .attr("font-size", 9)
+	    .text(function(d) { console.log(d); return d})
+	    .attr("font-weight", function(d,i) { if (i == 0) {return "bold"} else {return "normal"}});
+
+	for (var j= 0; j < related.length; j++) {
+	    plot.append("g").selectAll("circle").data(appearances[related[j]]).enter()
+		.append("circle")
+		.attr("cx", function(d,i) {
+		    return x(1+d.sceneStart) })
+		.attr("cy", function(d,i) {return y(relationCount - j) })
+
+		.attr("r", function(d) {
+		    if (sceneContainsCharacter(d, mainCharacter)) {
+			return x(0.2) - x(0)
+		    } else {
+			return x(0.15) - x(0)
+		    }
+		})
+
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+		.style("fill", function(d) {
+		    if (sceneContainsCharacter(d, mainCharacter)) {
+			return "#000000"
+		    } else {
+			return "#bbbbbb"
+		    }
+		})
+		.style("opacity",
+		       function(d) {
+			   if (sceneContainsCharacter(d, mainCharacter)) {
+			       return 1
+			   } else {
+			       return 0.25
+			   }
+		       })
+		.style("cursor", "pointer")
+
+
+	}
+
+	console.log("done")
+
+
+
+
+    }
+
+var ready = function(_, episodes, groups, characterData) {
+    episodeData = episodes
+	appearances = populateAppearances(episodeData.episodes)
+	characterDetails = getCharacterDetailsMap(characterData)
+
 	console.log(episodes)
 	console.log(groups)
 	populateCharacterHouseMap(groups.groups)
 	generateHouseLegend()
 	console.log(characterHouseMap)
 	generateCharacterCountPlot(episodes);
+
     }
 
     d3.queue()
 	.defer(d3.json, 'data/episodes.json')
 	.defer(d3.json, 'data/characters-groups.json')
+	.defer(d3.json, 'data/characters.json')
 	.await(ready);
